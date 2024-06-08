3af73b15c0e37e6813e1f56742550543fb7c425b
Lennart Brandin <lennart.brandin@tuhh.de>
2024-06-09 00:22:48 +0200

    Implement file download

--- studip.py
+++ studip.py
@@ -31,17 +31,21 @@ class StudIP:
             # Get course root folder instead of course folder
             course_root_folder = c.get("/folders")[0]["relationships"]["folders"]["links"]["related"]
             f = StudIPFolder(self, course_root_folder, "courses")
+            f.download()
             self.courses.append([c, f])
 
     def get_absolute(self, path):
         response = self.session.get(self.studip.root_url + path)
         if response.status_code != 200:
             print(f"{self.studip.root_url + path} {response.text}")
-            return []
+            return {}
         return response.json()["data"]
 
     def get_api(self, route):
         return self.get_absolute(self.studip.base_route + self.studip.api_route + route)
+    
+    def warning(self, message):
+        print(f"WARNING: {message}")
 
 class StudIPPath(StudIP):
     def __init__(self, studip, path):
@@ -74,21 +78,44 @@ class StudIPFolder(StudIPPath):
         self.files = []
         self.get_files()
         self.folders = []
+        self.get_folders()
 
     def get_files(self):
         # There is only one file ref per folder
         for file in self.get("/file-refs"):
-            self.files.append(StudIPFile(self.studip, file["relationships"]["file"]["links"]["related"], self.folderpath))
+            if file["attributes"]["is-readable"] and file["attributes"]["is-downloadable"]:
+                self.files.append(StudIPFile_ref(self.studip, file["links"]["self"], self.folderpath))
+            else:
+                self.warning(f"File {file['attributes']['name']} is not readable or downloadable")
 
     def get_folders(self):
-        for folder in self.get():
-            print(folder["attributes"]["name"])
-            self.folders.append(StudIPFolder(self.studip, folder["relationships"]["folders"]["links"]["related"]), self.folderpath)
+        for folder in self.get("/folders"):
+            if folder["attributes"]["is-readable"]:
+                self.folders.append(StudIPFolder(self.studip, folder["relationships"]["folders"]["links"]["related"], self.folderpath))
+            else:
+                self.warning(f"Folder {folder['attributes']['name']} is not readable")
 
-class StudIPFile(StudIPPath):
-    def __init__(self, studip, path, filepath):
+    def download(self):
+        if not os.path.exists(self.folderpath):
+            os.makedirs(self.folderpath)
+        for file in self.files:
+            file.download()
+        for folder in self.folders:
+            folder.download()
+
+class StudIPFile_ref(StudIPPath):
+    def __init__(self, studip, path, folderpath):
         super().__init__(studip, path)
-        print(filepath + "/" + self.attributes["name"])
+        self.filepath = folderpath + "/" + self.attributes["name"]
+
+    def download(self):
+        response = self.session.get(self.studip.root_url + self.path + "/content")
+        if response.status_code != 200:
+            self.warning(f"Download failed: {response.text}")
+            return
+        with open(self.filepath, "wb") as file:
+            file.write(response.content)
+        print(f"Downloaded {self.filepath}")
 
 def main():
     url = "https://e-learning.tuhh.de/studip"
