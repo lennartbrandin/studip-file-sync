c0e8ff61f093f3d0d82b9a57b32827d58e849421
Lennart Brandin <lennart.brandin@tuhh.de>
2024-06-09 16:05:32 +0200

    Some optimisation

--- studip.py
+++ studip.py
@@ -36,100 +36,84 @@ class StudIP:
         self.setup()
 
     def setup(self):
-        self.user = StudIPObjekt(self, self.get_api("/user/me"))
+        self.user = StudIPRoute(self, self.get_raw_api("/users/me").json()["data"])
 
         # Collect user course list
         self.courses = []
-        for course in self.user.get("/courses"):
-            c = StudIPObjekt(self, "/courses/" + course["id"])
+        for course in self.user.get_sub("/courses"):
+            c = StudIPRoute(self, course)
             print(f"Course: {c.attributes['title']}")
             # Get course root folder instead of course folder
-            course_root_folder = c.get("/folders")[0]["relationships"]["folders"]["links"]["related"]
-            f = StudIPFolder(self, course_root_folder, "courses")
+            f = StudIPFolder(self, c.get_sub("/folders")[0], "./courses")
             f.download()
             self.courses.append([c, f])
 
-    def get_absolute(self, path):
+    def get_raw_absolute(self, path, additional_headers):
         """Get json data from root_url + path"""
-        response = self.session.get(self.studip.root_url + path)
+        headers = self.session.headers.copy()
+        headers.update(additional_headers)
+        response = self.session.get(self.studip.root_url + path, headers=headers)
         if response.status_code != 200:
             print(f"{self.studip.root_url + path} {response.text}")
-            return {}
-        return response.json()["data"]
-
-    def get_api(self, route):
-        """Get json data from root_url + api_route + route"""
-        return self.get_absolute(self.studip.base_route + self.studip.api_route + route)
+        return response
+    
+    def get_raw_api(self, route, headers=""):
+        """Get root_url + api_route + route"""
+        return self.get_raw_absolute(self.studip.base_route + self.studip.api_route + route, headers)
     
     def warning(self, message):
         print(f"WARNING: {message}")
 
-class StudIPObjekt(StudIP):
-    """Object that behave relative to route"""
+class StudIPRoute(StudIP):
+    """Object that behaves relative to route"""
     def __init__(self, studip, data):
         self.studip = studip
         self.session = studip.session
         for key, value in data.items():
             setattr(self, key, value)
 
-    def get(self, route=""):
-        return self.get_api(self.type + "/" + self.id + route)
+    def get_raw_sub(self, route="", headers=""):
+        return self.get_raw_api("/"+ self.type +"/"+ self.id + route, headers)
+
+    def get_sub(self, route="", headers=""):
+        return self.get_raw_sub(route, headers).json()["data"]
     
-class StudIPPath(StudIP):
-    def __init__(self, studip, path):
-        self.studip = studip
-        self.session = studip.session
-        self.path = path
-        for key, value in self.get().items():
-            setattr(self, key, value)
-
-    def set_path(self, path):
-        self.path = path
-
-    def get(self, route=""):
-        return self.get_absolute(self.path + route)
-
-# Recursive folder/file structure
-class StudIPFolder(StudIPPath):
-    def __init__(self, studip, path, folderpath):
-        # Remove trailing section
-        path = "/".join(path.split("/")[:-1])
-        super().__init__(studip, path)
-        self.folderpath = folderpath + "/" + self.attributes["name"]
-        self.files = []
-        self.get_files()
+class StudIPFolder(StudIPRoute):
+    def __init__(self, studip, data, folder_path):
+        super().__init__(studip, data)
+        self.folder_path = folder_path + "/" + self.attributes["name"]
+        self.files_refs = []
         self.folders = []
+        self.get_file_refs()
         self.get_folders()
 
-    def get_files(self):
-        # There is only one file ref per folder
-        for file in self.get("/file-refs"):
-            if file["attributes"]["is-readable"] and file["attributes"]["is-downloadable"]:
-                self.files.append(StudIPFile_ref(self.studip, file["links"]["self"], self.folderpath))
+    def get_file_refs(self):
+        for file_ref in self.get_sub("/file-refs"):
+            if file_ref["attributes"]["is-readable"] and file_ref["attributes"]["is-downloadable"]:
+                self.files_refs.append(StudIPFile_ref(self.studip, file_ref, self.folder_path))
             else:
-                self.warning(f"File {file['attributes']['name']} is not readable or downloadable")
+                self.warning(f"File {file_ref['attributes']['name']} is not readable or downloadable")
 
     def get_folders(self):
-        for folder in self.get("/folders"):
+        for folder in self.get_sub("/folders"):
             if folder["attributes"]["is-readable"]:
-                self.folders.append(StudIPFolder(self.studip, folder["relationships"]["folders"]["links"]["related"], self.folderpath))
+                self.folders.append(StudIPFolder(self.studip, folder, self.folder_path))            
             else:
                 self.warning(f"Folder {folder['attributes']['name']} is not readable")
 
     def download(self):
-        if not os.path.exists(self.folderpath):
+        if not os.path.exists(self.folder_path):
             os.makedirs(self.folderpath)
-        for file in self.files:
-            file.download()
+        for file_ref in self.files_refs:
+            file_ref.download()
         for folder in self.folders:
             folder.download()
 
-class StudIPFile_ref(StudIPPath):
-    def __init__(self, studip, path, folder_path):
-        super().__init__(studip, path)
+class StudIPFile_ref(StudIPRoute):
+    def __init__(self, studip, data, folder_path):
+        super().__init__(studip, data)
         self.file_path = folder_path + "/" + self.attributes["name"]
         self.etag_path = self.file_path + ".etag"
-        self.download_url = self.studip.root_url + self.path + "/content"
 
     def download(self):
         if os.path.exists(self.file_path) and os.path.exists(self.etag_path):
@@ -139,7 +123,7 @@ class StudIPFile_ref(StudIPPath):
             self.download_etag("")
 
     def download_etag(self, etag):
-        response = self.session.get(self.download_url, headers={"If-None-Match": etag})
+        response = self.get_raw_sub("/content", headers={"If-None-Match": etag})
         if response.status_code == 200:
             with open(self.file_path, "wb") as f:
                 f.write(response.content)
@@ -155,5 +139,4 @@ def main():
     credentials = json.load(open("credentials.json"))
     studip_db = db("studip.db")
     studip = StudIP(studip_db, url, credentials["login"], credentials["password"])
-
 main()
